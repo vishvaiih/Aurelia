c4eb3fa1f50f349b7185b1fde23eab2c053c4bbb
vishvaiih <vishva.iihglobal@gmail.com>
2026-01-16 17:56:39 +0530

    continue footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,38 @@
 import { Box, Typography } from "@mui/material";
 import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import {
+  Facebook,Twitter
+} from "lucide-react";
 
 function Footer() {
   return (
   
     <>
-      <Box sx={{minHeight:"60vh",backgroundColor:"#2a2622"}}>
+      <Box sx={{minHeight:"40vh",backgroundColor:"#2a2622",padding:"70px"}}>
           
-          
+          <Box sx={{width:"25%"}}>
+               <Typography sx={{color:"#e0c685",fontFamily:"serif",fontSize:"21px"}}>Aurelia</Typography> 
+               <Typography sx={{color:"#a5a1a0",fontSize:"15px",marginTop:"13px",fontSize:"17px"}}> Crafting timeless jewelry pieces that celebrate life's precious moments since 1987.</Typography>
+               <Box sx={{display:"flex",color:"#a5a1a0",marginTop:"18px"}}>
+                      <InstagramIcon sx={{marginRight:"10px"}} />
+                      <Facebook style={{marginRight:"10px"}}/>
+                      <Twitter style={{marginRight:"10px"}}/>
+               </Box>
+                
+          </Box>
+
+          <Box>
+
+          </Box>
+
+          <Box>
+
+          </Box>
+
+          <Box>
+
+          </Box>
 
       </Box>
       
