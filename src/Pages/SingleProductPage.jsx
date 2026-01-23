f0263b5a117ceaf78b9018b4ba74df69f1b43c1d
vishvaiih <vishva.iihglobal@gmail.com>
2026-01-23 17:12:29 +0530

    start productDetailPage

--- src/Pages/SingleProductPage.jsx
+++ src/Pages/SingleProductPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import { Typography,Box } from '@mui/material';
 
 function SingleProductPage() {
   return (
     <>
-    
+    <Header/>
+      <Box sx={{color:"#fbfaf8"}}>
+          <Box sx={{display:"flex",color:"#8a8075",margin:" 30px 60px",alignItems:"center"}}>
+            <ArrowBackIcon sx={{fontSize:"17px",marginRight:"8px",fontWeight:"500"}}/>
+             <Typography>Back to Shop </Typography>
+      
+          </Box>
+
+
+      </Box>
+
+
+     <Footer/>
     
     </>
 
